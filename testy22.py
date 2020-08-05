292f6953b863c6e648ddccd8c2751dca26f64387
jungeric4 <>
2020-08-05 16:02:38 -0400

    help!

--- testy22.py
+++ testy22.py
@@ -3,9 +3,12 @@ from sympy import *
 init_printing(use_unicode=True)
 
 
-V18d, C18, R13, R15, R19, V18, R16, R14, V10, V12 = symbols('V18d, C18, R13, R15, R19, V18, R16, R14, V10, V12')
+V18d, C18, R13, R15, R19, V18, R16, R14, R5, R3, R4, R2, R9, R8, R7, V4r, V5r, V1 = symbols('V18d, C18, R13, R15, R19, V18, R16, R14, R5, R3, R4, R2, R9, R8, R7. V4r, V5r, V1')
 M = Matrix([V18d])
 A = (-1) / (C18 * (R13 + R15)) - (1) / (C18 * R19)
 q = Matrix([V18])
 B = (1) / (C18 * (R13 + R15)) - (1) / (C18 * R13) * (R16) / (C18 * R13 * (R14 + R16))
-u = Matrix([V10, V12])
+u = Matrix([V1])
+V4r = (R4) / (R4 + R2)
+V5r = (R5) / (R5 + R3)
+MadCuzBad = (V5r + (V4r - V5r)(R9/R8)) + (V4r - (V4r - V5r)(R7/R8))
